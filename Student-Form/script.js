e1a2447f48c0a53c3ed2045e7443678a48f6cc2b
chrislerlavisorescaraecle-dotcom <chrislerlavisores.caraecle@my.smciligan.edu.ph>
2026-08-10 21:45:24 +0800

    Add files via upload

---
+++ Student-Form/script.js
@@ -0,0 +1,31 @@
+const contactForm = document.getElementById("contactForm");
+const pageHeading = document.getElementById("pageHeading");
+const confirmation = document.getElementById("confirmation");
+
+contactForm.addEventListener("submit", function(event) {
+
+    event.preventDefault();
+
+    const name = document.getElementById("name").value.trim();
+
+    const email = document.getElementById("email").value.trim();
+
+    const message = document.getElementById("message").value.trim();
+
+
+    if (name === "" || email === "" || message === "") {
+
+        confirmation.textContent = "Please complete all fields.";
+        
+        return;
+    }
+
+    confirmation.textContent =
+        "Your message has been successfully submitted!";
+
+    pageHeading.style.color = "black";
+
+    pageHeading.textContent = "Thank you for your message!";
+
+    contactForm.reset();
+}); 
